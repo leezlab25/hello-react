04726600bbf9aa419e512213566505ce7428e9f8
이영중 <sigijini.lee@gmail.com>
2026-07-30 23:31:42 +0900

    컴포넌트 생성 실습

---
+++ src/MyComponent.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const MyComponent = ({name="기본이름", favoriteNumber, children}) => {
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name} 입니다. <br />
+            Children 값은 {children}
+            입니다.
+            <br />
+            제가 좋아하는 숫자는 {favoriteNumber}입니다.
+        </div>
+    )
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+};
+export default MyComponent;
