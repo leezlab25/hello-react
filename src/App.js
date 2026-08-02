9a872ea332b1190e467a7ae6bfe3deae87f83ff7
이영중 <sigijini.lee@gmail.com>
2026-08-02 22:19:14 +0900

    13장. 리액트 라우터로 SPA 개발하기

--- src/App.js
+++ src/App.js
@@ -1,72 +1,30 @@
-import { produce } from 'immer';
-import {useRef, useCallback, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
+import NotFound from './pages/NotFound';
 
 const App = () => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({name: '', username: ''});
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null
-  });
-
-  // input 수정을 위한 함수
-  const onChange = useCallback((e) => {
-    const {name, value} = e.target;
-    setForm(
-      produce(form, draft => {
-        draft[name] = value;
-      }));
-  }, [form])
-  
-  // form 등록을 위한 함수
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
-    const info = {
-      id: nextId.current,
-      name: form.name,
-      username: form.username
-    };
-
-    // array에 새항목 등록
-    setData(
-      produce(data, draft => {
-        draft.array.push(info);
-      })
-    );
-
-    // from 초기화
-    setForm({
-      name: '',
-      username: ''
-    });
-    nextId.current += 1;
-  }, [data, form.name, form.username]);
-
-  // 항목을 삭제하는 함수
-  const onRemove = useCallback((id) => {
-    setData(
-      produce(data, draft => {
-        draft.array.splice(draft.array.findIndex(info => info.id === id), 1)
-      })
-    );
-  }, [data]);
-
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input name='username' placeholder='아이디' value={form.username} onChange={onChange} />
-        <input name='name' placeholder='이름' value={form.name} onChange={onChange} />
-        <button type="submit">등록</button>
-      </form>
-      <div>
-        <ul>
-          {data.array.map((info) => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username} ({info.name})
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Routes>
+        <Route element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
+        <Route path="/articles" element={<Articles />} >
+          <Route path=":id" element={<Article />} />
+        </Route>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
